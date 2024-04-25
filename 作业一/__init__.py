d2325c6209efed6ca57cb39f5fe3075a1d488369
mango7789 <981653507@qq.com>
2024-04-25 21:14:33 +0800

    hw1: finish the training section of report

--- 作业一/__init__.py
+++ 作业一/__init__.py
@@ -6,6 +6,8 @@ import urllib.request
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from typing import List, Dict, Literal, Callable, Final
 from tqdm import tqdm
-plt.rcParams['font.size'] = 12
+plt.rcParams['font.size'] = 14
+sns.set_theme()
