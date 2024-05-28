cf0266318b98ec7a679bb619fef4695bee16d15e
mango7789 <981653507@qq.com>
2024-05-28 21:07:01 +0800

    VOC: modify the config for yolov3

--- 期中作业/Pascal VOC/configs/pascal_voc/yolov3.py
+++ 期中作业/Pascal VOC/configs/pascal_voc/yolov3.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/default_runtime.py', '../_base_/datasets/voc0712.py'
+     '../_base_/datasets/voc0712.py', '../_base_/default_runtime.py'
 ]
 # model settings
 model = dict(
@@ -58,142 +58,13 @@ model = dict(
         nms=dict(type='nms', iou_threshold=0.45),
         max_per_img=100))
 
-# dataset settings
-dataset_type = 'VOCDataset'
-data_root = 'vocdata/VOCdevkit/'
-img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile', to_float32=True),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Expand',
-        mean=img_norm_cfg['mean'],
-        to_rgb=img_norm_cfg['to_rgb'],
-        ratio_range=(1, 2)),
-    dict(
-        type='MinIoURandomCrop',
-        min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
-        min_crop_size=0.3),
-    dict(type='Resize', img_scale=[(320, 320), (608, 608)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='PhotoMetricDistortion'),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(608, 608),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
-        ])
-]
-
-# data = dict(
-#     samples_per_gpu=4,
-#     workers_per_gpu=4,
-#     train=dict(
-#         type='RepeatDataset',
-#         times=3,
-#         dataset=dict(
-#             type=dataset_type,
-#             ann_file=[
-#                 data_root + 'VOC2007/ImageSets/Main/train.txt',
-#             ],
-#             img_prefix=[data_root + 'VOC2007/'],
-#             pipeline=train_pipeline)),
-#     val=dict(
-#         type=dataset_type,
-#         ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-#         img_prefix=data_root + 'VOC2007/',
-#         pipeline=test_pipeline),
-#     test=dict(
-#         type=dataset_type,
-#         ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-#         img_prefix=data_root + 'VOC2007/',
-#         pipeline=test_pipeline))
-# # optimizer
-# optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0005)
-# optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-# # learning policy
-# lr_config = dict(
-#     policy='step',
-#     warmup='linear',
-#     warmup_iters=2000,  # same as burn-in in darknet
-#     warmup_ratio=0.1,
-#     step=[218, 246])
-# # runtime settings
-# runner = dict(type='EpochBasedRunner', max_epochs=10)
-# evaluation = dict(interval=1, metric=['mAP'])
-
-# data path
-dataset_type = 'VOCDataset'
-data_root = '/kaggle/input/vocdata/VOCdevkit/'
-# Train dataloader configuration
-train_dataloader = dict(
-    batch_size=4,
-    num_workers=4,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
-    batch_sampler=dict(type='AspectRatioBatchSampler'),
-    dataset=dict(
-        type='RepeatDataset',
-        times=3,
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            ann_file='VOC2007/ImageSets/Main/train.txt',
-            data_prefix=dict(img='VOC2007/'),
-            filter_cfg=dict(filter_empty_gt=True, min_size=32),
-            pipeline=train_pipeline
-        )
-    )
-)
-
-# Validation dataloader configuration
-val_dataloader = dict(
-    batch_size=1,
-    num_workers=2,
-    persistent_workers=True,
-    drop_last=False,
-    sampler=dict(type='DefaultSampler', shuffle=False),
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='VOC2007/ImageSets/Main/test.txt',
-        data_prefix=dict(img='VOC2007/'),
-        test_mode=True,
-        pipeline=test_pipeline
-    )
-)
-
-# Test dataloader configuration
-test_dataloader = val_dataloader
-
-# Evaluation configurations
-val_evaluator = dict(
-    type='VOCMetric',
-    ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-    metric='mAP'
-)
-test_evaluator = val_evaluator
-
 # Training configuration
-train_cfg = dict(max_epochs=10, val_interval=1)
+train_cfg = dict(max_epochs=273, val_interval=7)
 
 # Optimizer configuration
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0005),
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005),
     clip_grad=dict(max_norm=35, norm_type=2)
 )
 
@@ -204,7 +75,7 @@ param_scheduler = [
 ]
 
 # Hooks configuration
-default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=1))
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=7))
 
 # Auto scaling learning rate configuration
 auto_scale_lr = dict(base_batch_size=64)
