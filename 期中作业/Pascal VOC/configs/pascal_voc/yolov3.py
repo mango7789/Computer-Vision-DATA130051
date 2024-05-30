f509172bbd9631dd9b009e0b33b587b1a5294652
mango7789 <981653507@qq.com>
2024-05-30 21:00:09 +0800

    VOC: add yolov3 pth

--- 期中作业/Pascal VOC/configs/pascal_voc/yolov3.py
+++ 期中作业/Pascal VOC/configs/pascal_voc/yolov3.py
@@ -73,13 +73,13 @@ test_cfg = dict(type='TestLoop')
 # Optimizer configuration
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0005),
+    optimizer=dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0005),
     clip_grad=dict(max_norm=35, norm_type=2)
 )
 
 # Learning rate scheduler configuration
 param_scheduler = [
-    dict(type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=2000),
+    dict(type='LinearLR', start_factor=0.0005, by_epoch=False, begin=0, end=2000),
     dict(type='MultiStepLR', by_epoch=True, milestones=[4, 8], gamma=0.1)
 ]
 
